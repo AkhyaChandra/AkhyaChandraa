4bb874fc33274234a9a861a5437180fb09aee056
Akhya Chandra <77987097+AkhyaChandra@users.noreply.github.com>
2023-10-05 20:13:49 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed=new Typed(".text", {
-  strings:["Data Analyst", "Web Developer", "Youtuber"],
+  strings:["Data Analyst", "Web Developer", "Artist"],
   typeSpeed: 100,
   backSpeed: 100,
   backDelay: 1000,
